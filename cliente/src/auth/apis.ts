18fcfb5e218f67a299be96ef2f3fd7169122423b
DiegoLorga <ramseslorga@gmail.com>
2024-08-02 13:56:56 -0600

    cambio de api

--- cliente/src/auth/apis.ts
+++ cliente/src/auth/apis.ts
@@ -1,4 +1,4 @@
 export const API_URL = "http://localhost:3000/api"
 export const API_URI_IMAGENES = "http://localhost:3002/api"
-export const API_URI_CORREOS = "http://localhost:3001/api"
+export const API_URI_CORREOS = "http://localhost:3001"
 
